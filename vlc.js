0334feda89755a71ddd5b2011e797ec6dad8a3b7
Thiago Amarante <thiagoaag@gmail.com>
2014-12-23 18:05:07 -0200

    update vlc.js

--- vlc.js
+++ vlc.js
@@ -1,38 +1,57 @@
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 	
 module.exports = function(options, callback){
 
-var webSource = options.webSource || "record";
-	webSource = webSource.replace(/\s/g, "");
+	var logText = "";
 
-var url = options.url;
+	var webSource = options.webSource || "record";
+		webSource = webSource.replace(/\s/g, "");
+		webSource = webSource.replace(/\./g, "-");
 
-if(url === undefined || url.trim() == ""){
-	return callback('Failed: url not defined');
-}
+	var timeRecord = options.time || 5; //seconds
+
+	var url = options.url;
+
+	if(url === undefined || url.trim() == ""){
+		return callback("Failed: url not defined\n");
+	}
 	
-var timeRecord = options.time || 5;
+	var dateNow = new Date().toISOString().slice(0, 23).replace("T","_").replace(".", "");
 	
-var path = options.path || "/tmp/" + webSource + "_" + new Date().toISOString().slice(0, 23).replace("T","_") + ".ogg";	
-	
-var logText = "";
+	var fileName =  webSource + "_" + dateNow + ".ogg";
 
+	var path = options.path;
 
-var cmd = "cvlc -vvv";
+	if(path === undefined || path.trim() == ""){
+		path = "/tmp/" + fileName;
+	}else if(!fs.existsSync(path)){
+
+		fs.mkdirSync(path, 0766, function(err) {
+			if(err){
+				return callback("Failed: can't make the directory \n");
+			}
+		});
+	}else{
+		path += "/" + fileName;
+	}
+		
+	var cmd = "cvlc -vvv";
 	cmd += " --run-time=" + timeRecord;
-	//cmd += " --stop-time=" + timeRecord;
+	if( url.indexOf("rtsp") == -1){ 
+		cmd += " --stop-time=" + timeRecord; }	
 	cmd +=  " " + url;
 	cmd += " --sout='#transcode{acodec=vorbi,ab=128,channels=1,samplerate=44100}:";
 	cmd += "std{access=file,mux=ogg,dst=" + path + "}'";
 	cmd += " vlc://quit";
 	
-	//console.log("cmd: " + cmd);
+	//log("cmd: " + cmd);
 
 	var vlc = exec(cmd, function(error, stdout, stderr){
 
 		if(error){
-			//console.log(error.stack);
+			//log(error.stack);
 			console.log('Error code: ' + error.code);
 			return callback('Failed: command [clvc] not found');
 		}
@@ -45,9 +64,7 @@ var cmd = "cvlc -vvv";
 	});
 	
 	vlc.stderr.on('data', function(data){
-			
 		//log("STDERR: " + data);
-		
 		logText += data;
 
 		if(data.indexOf('writing header') != -1 ){
